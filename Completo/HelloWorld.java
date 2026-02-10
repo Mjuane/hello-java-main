90e657ea88162ae098c110b16cf0b59278065351
Miguel <mjuane152@gmail.com>
2026-02-11 00:11:17 +0100

    Comments and HelloWorld finished

--- Completo/HelloWorld.java
+++ Completo/HelloWorld.java
@@ -1,3 +1,13 @@
+package Completo;
+
+// commentario de una sola linea
+
+/*
+comentario de varias lineas
+
+comentario de varias lineas
+*/
+
 public class HelloWorld {
     
     public static void main(String[] args) {
