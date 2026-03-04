324e369f35ba2924dc1c2bb08ee77b9a59a2793d
Miguel <mjuane152@gmail.com>
2026-03-04 11:58:51 +0100

    OperatorsExercises 2

--- basic/c02_operators/OperatorsExercises.java
+++ basic/c02_operators/OperatorsExercises.java
@@ -21,6 +21,31 @@ public class OperatorsExercises {
 
         // 2. Crea una variable para cada tipo de operación de asignación.
 
+        var b = 5;
+
+        System.out.println(b);
+
+        b -= 5;
+
+        System.out.println(b);
+
+        b += 5;
+
+        System.out.println(b);
+
+        b *= 5;
+
+        System.out.println(b);
+
+        b /= 5;
+
+        System.out.println(b);
+
+        b %= 5;
+
+        System.out.println(b);
+
+
         // 3. Imprime 3 comparaciones verdaderas con diferentes operadores de comparación.
 
         // 4. Imprime 3 comparaciones falsas con diferentes operadores de comparación.
